e04e7a5455c52f2bb3643d3aa86bf1155c433733
Sk Saif Ahmed <sksaifahmed0@gmail.com>
2022-01-07 18:40:58 +0530

    format code

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 class Calculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Enter a number: ");
+
+		System.out.print("Enter a number: ");
 		int a = scanner.nextInt();
 
-		System.out.println("Enter another number: ");	
+		System.out.print("Enter another number: ");
 		int b = scanner.nextInt();
 
-		System.out.println("Enter the operator: ");
-		char operator = scanner.next().charAt(0);	
+		System.out.print("Enter the operator: ");
+		char operator = scanner.next().charAt(0);
 
-		int result = 0;		
-		
+		int result = 0;
+
 		switch(operator) {
 			case '+':
 				result = a + b;
@@ -31,12 +31,12 @@ class Calculator {
 			case '%':
 				result = a % b;
 				break;
-			default: 
+			default:
 				System.out.println("Wrong Input!");
 				System.exit(1);
 		}
-			
-		System.out.println("Result = " + result);	
+
+		System.out.println("Result = " + result);
 
 	}
 }
